e558cfe5c661df09ab5d08edee7d8d91b4709c0e
Azxma <al158790@alumnos.uacj.mx>
2021-10-29 22:47:29 -0600

    [ADD] Agrega funcionalidad del camarada y agrega ejempo

--- Assets/prefabs/plataforma/plataforma.cs
+++ Assets/prefabs/plataforma/plataforma.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class plataforma: MonoBehaviour{
+    public GameObject camarada;
+
     public bool es_una_ilusion = false;
 
     void Start(){
         if(es_una_ilusion)
             convertir_a_iusion();
+
+        if(camarada != null) {
+            camarada.SetActive(false);
+        }
     }
 
     void Update(){
@@ -24,5 +30,20 @@ public class plataforma: MonoBehaviour{
         if(otro.gameObject.CompareTag("Jugador")) {
             GetComponent<Animation>().Play();
         }
+    }
+
+    public void mostrar_camarada() {
+        if(es_una_ilusion || camarada == null)
+            return;
+
+        camarada.SetActive(true);
+
+        Animation anima = camarada.GetComponent<Animation>();
+        
+        if(anima != null)
+            anima.Play();
+
+        else
+            Debug.LogError("El objeto debe tener un animation con un clip para poder mostrar y activar. Por favor añadir uno.");
     }
 }
