ff79e0722b9c3975132cd17bf75d75a964ab4457
Azxma <al158790@alumnos.uacj.mx>
2021-10-30 04:58:26 -0600

    {CTR} Modifica comportamiento de salto de jugador

--- Assets/prefabs/jugador/Jugador.cs
+++ Assets/prefabs/jugador/Jugador.cs
@@ -10,6 +10,8 @@ public class Jugador: MonoBehaviour{
     private float direccion;
     private float caida_fuerza;
 
+    private bool pre_fase;
+
     private CharacterController controlador;
 
     void Start(){
@@ -18,8 +20,8 @@ public class Jugador: MonoBehaviour{
         caida_fuerza = -gravedad;
     }
     
-    private void Update() {
-         if(Input.GetKeyDown(KeyCode.Space) && controlador.isGrounded) {
+    void Update() {
+         if(Input.GetKey(KeyCode.Space) && controlador.isGrounded) {
             saltar();
          }
     }
@@ -31,19 +33,25 @@ public class Jugador: MonoBehaviour{
     }
 
     void moverse(){
-        controlador.Move(new Vector3(Input.GetAxis("Horizontal") * velocidad, calcular_fuerza_gravedad(), 0));
+        controlador.Move(new Vector3(direccion * velocidad, calcular_fuerza_gravedad(), 0));
+        Debug.Log("--> " + caida_fuerza);
+        Debug.Log("--> " + pre_fase);
+
     }
 
     void saltar(){
-        caida_fuerza = salto_fuerza;
+        Debug.Log("--> <--");
+        pre_fase = true;
+        caida_fuerza = 2F;
         moverse();
     }
 
     float calcular_fuerza_gravedad(){
-        //if((controlador.isGrounded || caida_fuerza < -gravedad) && caida_fuerza < salto_fuerza - 15F) {
-        if((controlador.isGrounded || caida_fuerza < -gravedad * 4) && caida_fuerza < salto_fuerza - 2F) {
-            if(controlador.isGrounded) 
+        if((controlador.isGrounded || caida_fuerza < -gravedad * 4) && !pre_fase) {
+            if(controlador.isGrounded){ 
                 caida_fuerza = 0;
+                pre_fase = false;
+            }
 
             else if(caida_fuerza < -gravedad * 4)
                 caida_fuerza = -gravedad * 4;
@@ -51,7 +59,16 @@ public class Jugador: MonoBehaviour{
             return caida_fuerza;
         }
 
-        if(controlador.velocity.y > -gravedad){
+        if(pre_fase) {
+            caida_fuerza += (salto_fuerza * Time.deltaTime);
+        }
+
+        if(caida_fuerza > salto_fuerza && pre_fase) {
+            pre_fase = false;
+        }
+
+        
+        if(!pre_fase && controlador.velocity.y > -gravedad){
             caida_fuerza -= (gravedad * Time.deltaTime);
         }
 
